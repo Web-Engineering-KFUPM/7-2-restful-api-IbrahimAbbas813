c16ceb3971d2de8b4bc046acc6120a96bccb9446
IbrahimAbbas813 <s202178330@kfupm.edu.sa>
2026-04-15 10:32:07 +0300

    TODO 3

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// db schema
 const songSchema = new mongoose.Schema({
   title:  { type: String, required: true, trim: true },
   artist: { type: String, required: true, trim: true },
